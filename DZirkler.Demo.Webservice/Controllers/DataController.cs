57a404c92bf08ae8a41287524f8a8c641f9b6bd7
Damon Zirkler <dzirkler@steelcase.com>
2017-04-09 21:12:34 -0700

    add data container

---
+++ DZirkler.Demo.Webservice/Controllers/DataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DZirkler.Demo.Webservice.Controllers
+{
+	[Route("api/v1/[controller]")]
+	public class DataController : Controller
+	{
+		// GET api/hello
+		[HttpGet]
+		public List<string> Get()
+		{
+			string filecontents = System.IO.File.ReadAllText("/data_files/data.txt");
+			List<string> items = new List<string>(filecontents.Split("\n".ToCharArray()));
+
+			return items;
+		}
+
+	}
+}
---
+++ Data.Dockerfile
@@ -0,0 +1,7 @@
+﻿FROM alpine:3.5
+
+VOLUME /data_files
+
+COPY ./data.txt /data_files/data.txt
+
+CMD /bin/true
