1759fe37fdd88d481ae0614f80164a77ff83ec43
Damon Zirkler <dzirkler@steelcase.com>
2017-04-13 20:15:02 -0700

    modify script

--- build_data_image.sh
+++ build_data_image.sh
@@ -5,8 +5,12 @@ echo "New Data Line: $(date -R)" >> data.txt
 NOW=$(date +'%Y-%m-%dT%H%M%S')
 TAGNAME="dzirkler/cf-dockercon-demo-data:$NOW"
 
-docker build -f ./Data.Dockerfile --tag $TAGNAME .
+docker build -f ./Data.Dockerfile --tag dzirkler/cf-dockercon-demo-data:latest .
+docker push dzirkler/cf-dockercon-demo-data:latest
+
+docker tag dzirkler/cf-dockercon-demo-data:latest $TAGNAME 
 docker push $TAGNAME 
-docker push "dzirkler/cf-dockercon-demo-data:latest"
+
+
 
 
